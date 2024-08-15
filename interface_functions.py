f70e3c8260970636c3727325560793436c2250a2
Moreno <biielmoreno475@gmail.com>
2024-08-15 18:28:31 -0300

    update1

--- interface_functions.py
+++ interface_functions.py
@@ -1,16 +1,50 @@
 import sys
+import time
+
 from PyQt6 import uic, QtWidgets
 from PyQt6.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout
 from PyQt6.QtGui import QDesktopServices
 from PyQt6.QtCore import QUrl
 
+
+
 app = QtWidgets.QApplication([])
 window = uic.loadUi('interface.ui')
 
-def open_webbrowser():
-        print('clicked')
-        url = QUrl("https://www.google.com")
+def start_timer():
+    print('sessss')
+    window.label_12.setText("00:00:00")
+
+    start_time = time.time()
+
+    while True:
+
+        elapsed_time = time.time() - start_time
+
+        hours, remainder = divmod(elapsed_time, 3600)
+
+        minutes, seconds = divmod(remainder, 60)
+
+        timer_text = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
+
+        window.label_12.setText(timer_text)
+
+        app.processEvents()  # necessário para atualizar a interface
+
+        time.sleep(0.1)  # atualiza a cada 0.1 segundos
+
+def open_github():
+
+        url = QUrl("https://github.com/gmoreno-dev")
         QDesktopServices.openUrl(url)
 
-def clickedButtons():
-    window.pushButton_9.clicked.connect(open_webbrowser)
+def open_linkedin():
+        
+        url = QUrl("https://www.linkedin.com/in/jgabriel-moreno-dev/")
+        QDesktopServices.openUrl(url)
+        
+def open_instagram():
+        
+        url = QUrl("https://www.instagram.com/gabmorenogab/")
+        QDesktopServices.openUrl(url)
+
--- main.py
+++ main.py
@@ -1,13 +1,50 @@
+import time
+
 from PyQt6 import uic, QtWidgets
+from PyQt6.QtGui import QDesktopServices
+from PyQt6.QtCore import QUrl
 from control_database import get_tasks_from_db, update_task_completion_status
 from datetime import date
-from interface_functions import clickedButtons
+from interface_functions import open_github, open_linkedin, open_instagram, start_timer
 import resources
 
 app = QtWidgets.QApplication([])
 window = uic.loadUi('interface.ui')
 
 table_values = get_tasks_from_db()
+
+def start_timer():
+    print('test')
+    window.label_12.setText("00:00:00")
+
+    start_time = time.time()
+
+    while True:
+
+        elapsed_time = time.time() - start_time
+
+        hours, remainder = divmod(elapsed_time, 3600)
+
+        minutes, seconds = divmod(remainder, 60)
+
+        timer_text = "{:02}:{:02}:{:02}".format(int(hours), int(minutes), int(seconds))
+
+        window.label_12.setText(timer_text)
+
+        app.processEvents()  # necessário para atualizar a interface
+
+        time.sleep(0.1)  # atualiza a cada 0.1 segundos
+
+def clickedButtons():
+
+    #Botoes Redes
+    window.pushButton_9.clicked.connect(open_github)
+    window.pushButton_10.clicked.connect(open_linkedin)
+    window.pushButton_11.clicked.connect(open_instagram)
+
+    #Botoes Timer
+    window.pushButton_6.clicked.connect(start_timer)
+
 
 #Alterar o texto da checkBox para os nomes na tabela da database task_manager.db
 def tasks_on_main_dashboard(table_values, max_length=11):
@@ -43,13 +80,13 @@ def setup_signals():
     window.checkBox_6.stateChanged.connect(lambda: update_task_completion_status(table_values[5][0], 1 if window.checkBox_6.isChecked() else 0))
 
 def main():
-
     clickedButtons()
     tasks_on_main_dashboard(table_values)
     setup_signals()
     set_date()
     window.show()
     app.exec()
+    
 
 if __name__ == '__main__':
     main()
